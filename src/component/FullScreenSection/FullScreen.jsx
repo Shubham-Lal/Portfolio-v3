49e342f2ed651aae7a2255fef5bc264fb3ea5c48
Shubham Lal <im.shubhamlal@gmail.com>
2024-02-14 21:49:13 +0530

    style prev & next button

--- src/component/FullScreenSection/FullScreen.jsx
+++ src/component/FullScreenSection/FullScreen.jsx
@@ -3,7 +3,6 @@ import { useContext, useEffect, useMemo, useState } from 'react'
 import { Context } from '../../Provider'
 import { photo } from '../../data/photo'
 import { graphic } from '../../data/graphic'
-import { BiSolidLeftArrow, BiSolidRightArrow } from 'react-icons/bi'
 
 const FullScreen = () => {
     const { setShowMedia, mediaSrc, setMediaSrc, } = useContext(Context);
@@ -80,20 +79,24 @@ const FullScreen = () => {
             >
                 <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" /></svg>
             </div>
-            <div>
+            <div className="prev-next__container">
                 <button
+                    className={`prev__btn ${mediaSrc.category === "photo" ? (
+                        currIndex >= photo.length - 1 && "hidden"
+                    ) : currIndex >= graphic.length - 1 && "hidden"}`}
                     onClick={handlePrev}
                     disabled={mediaSrc.category === "photo" ? (
                         currIndex >= photo.length - 1
                     ) : currIndex >= graphic.length - 1}
                 >
-                    <BiSolidLeftArrow />
+                    Prev
                 </button>
                 <button
+                    className={`next__btn ${currIndex <= 0 && "hidden"}`}
                     onClick={handleNext}
                     disabled={currIndex <= 0}
                 >
-                    <BiSolidRightArrow />
+                    Next
                 </button>
             </div>
             <div className="image__gallery">
